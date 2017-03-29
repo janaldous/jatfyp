3dc5187d9d09da4d54f1fee3dd2d915808ce10a1
janaldoustorres <jat.torres@gmail.com>
2017-03-29 21:47:48 +0100

    removed matplotlib from requirements

--- clusters/clustering.py
+++ clusters/clustering.py
@@ -1,7 +1,7 @@
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
